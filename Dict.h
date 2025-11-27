107994f98f80f7bb714536d67f6f00f4ff3e6df1
Paula <pbarter@upv.edu.es>
2025-11-27 18:08:48 +0100

    Añadido Makefile y código de test de la clase TableEntry

--- Dict.h
+++ Dict.h
@@ -6,10 +6,10 @@ using namespace std;
 template <typename V>
 class Dict {
     public: 
-        void insert(string key, V value); 
-        V search(string key); 
-        V remove(string key); 
-        int entries(); 
+        virtual void insert(string key, V value) = 0; 
+        virtual V search(string key) = 0; 
+        virtual V remove(string key) = 0; 
+        virtual int entries() = 0; 
 };
 
 
---
+++ TableEntry.h
@@ -0,0 +1,59 @@
+#ifndef TABLEENTRY_H
+#define TABLEENTRY_H
+using namespace std; 
+#include <string>
+#include <ostream>
+
+template <typename V> 
+class TableEntry {
+    public:
+        //ATRIBUTOS 
+        string key; 
+        V value; 
+
+
+        //METODOS
+
+        //Constructor
+        TableEntry(string key, V value){
+            this->key = key; 
+            this-> value = value; 
+        }
+
+        //Crea una entrada solo con clave (sin valor)
+        TableEntry(string key){
+            this->key = key; 
+            this->value = NULL; 
+        }
+
+        //Crea una entrada con clave "" (cadena vacía) y sin valor
+        TableEntry(){
+            this->key = ""; 
+            this->value = NULL; 
+        }
+
+        /*Sobrecarga global del operador == para determinar que dos 
+        instancias de TableEntry son iguales solo si comparten la misma 
+        clave (con independencia del valor).*/
+        friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
+            return te1.key == te2.key; 
+        }
+
+
+        /*Sobrecarga global del operador != para determinar que dos instancias 
+        de TableEntry son diferentes solo si sus claves son distintas 
+        (con independencia del valor)*/
+        friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
+            return te1.key != te2.key; 
+        }
+
+        /*Sobrecarga global del operador << para imprimir el contenido de la entrada 
+        (par clave->valor) por pantalla. Recuerda incluir la cabecera <ostream> en el .h.*/
+        friend ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
+            out << '{' << te.key << ':' << te.value << '}'; 
+            return out; 
+        }
+    
+};
+
+#endif
